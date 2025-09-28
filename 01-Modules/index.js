e68af4aa68e264a81fd2252ebeed46936cfc288a
subhadip07 <subhadipbag18@outlook.com>
2025-09-28 12:56:38 +0530

    update: index.js

--- 01-Modules/index.js
+++ 01-Modules/index.js
@@ -1,6 +1,20 @@
-const fs = require('fs'); // Built in module
+const fs = require('node:fs'); // Built in module
 // console.log(fs);
 
 // Read a file
 const content = fs.readFileSync('notes.txt', 'utf-8')
-console.log(content);
+// console.log(content);
+
+// Writing a file
+fs.writeFileSync('copy.txt', 'I want to write this', 'utf-8');
+
+// adding contents in a same file without overiding it
+fs.appendFileSync('copy.txt', content, 'utf-8');
+
+// Creating directory and folder inside
+fs.mkdirSync('games/xyx/a', {recursive: true});
+
+// Removing the directory
+fs.rmdirSync('games');
+
+fs.unlinkSync('copy.txt');
