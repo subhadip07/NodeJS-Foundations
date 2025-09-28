2cf07c019ad08a0fca83b351cb4930bee60a51d2
subhadip07 <subhadipbag18@outlook.com>
2025-09-28 22:25:48 +0530

    add: myErrorEvents.js

---
+++ 02-Events/myErrorEvents.js
@@ -0,0 +1,9 @@
+const { error } = require("console")
+const EventEmitter = require("events")
+const eventEmitter = new EventEmitter()
+
+eventEmitter.on('error', (err) => {
+    console.error(`Error Occurred: ${err.message}`)
+})
+
+eventEmitter.emit('error', new Error('Something went wrong'))
