9ffa1320e81a2783680919bc6e67531f0d9b6ae3
subhadip07 <subhadipbag18@outlook.com>
2025-09-28 22:12:47 +0530

    add: myEvents.js

---
+++ 02-Events/myEvents.js
@@ -0,0 +1,28 @@
+const EventEmitter = require("events");
+
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('greet', (username) => {
+    console.log(`Hello ${username} and welcome to events in node js`);
+    
+});
+
+eventEmitter.once('pushnotify', () => {
+    console.log("This event will run only once");
+});
+
+const myListener = () => console.log("I am a test listener");
+eventEmitter.on("test", myListener);
+
+
+// Emit the event
+eventEmitter.emit('greet', 'subh');
+
+eventEmitter.emit("pushnotify");
+eventEmitter.emit("pushnotify");
+
+eventEmitter.emit("test");
+eventEmitter.removeListener("test", myListener);
+eventEmitter.emit("test");
+
+console.log(eventEmitter.listeners("greet"));
