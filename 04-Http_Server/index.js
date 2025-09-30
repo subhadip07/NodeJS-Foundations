3569c51fd2c702015b891d28c2e75b496854bf0f
subhadip07 <subhadipbag18@outlook.com>
2025-09-30 13:05:18 +0530

    update: http_server/index.js

--- 04-Http_Server/index.js
+++ 04-Http_Server/index.js
@@ -1,13 +1,42 @@
 const http = require('node:http');
 
+// const server = http.createServer(function (req, res)
+// {
+//     console.log(`Incomming request at [${Date.now()}]`);
+//     console.log(req.headers);
+    
+//     res.writeHead(200);
+//     res.end('Ok!');
+// });
+
 const server = http.createServer(function (req, res)
 {
-    console.log('I got an incomming request');
-    res.writeHead(200);
-    res.end('Thanks for visiting my server');
+    console.log(`Incomming request at [${Date.now()}]`);
+    console.log(req.url);
+    
+    // ..
+    switch (req.url)
+    {
+        case '/':
+        res.writeHead(200);
+        return res.end(`Homepage`);
+
+    case '/contact-us':
+        res.writeHead(200);
+        return res.end(`Contact Me at xyz@gmail.com`);
+    
+    case '/about':
+        res.writeHead(200);
+        return res.end('I am a software eng');
+
+    default:
+        res.writeHead(404);
+        return res.end("You are lost");
+    }
+     
 });
 
-server.listen(8000, function()
+server.listen(8000, () =>
 {
-    console.log(`Http server is up and runningnon port 8000`);
+    console.log(`Http server is up and running on Port: 8000`);
 });
