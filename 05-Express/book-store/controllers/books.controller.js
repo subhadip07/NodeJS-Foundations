13055e5223679b301276d1d815e094b4c6b1a0e1
subhadip07 <subhadipbag18@outlook.com>
2025-10-06 22:42:21 +0530

    update: books.controller.js

--- 05-Express/book-store/controllers/books.controller.js
+++ 05-Express/book-store/controllers/books.controller.js
@@ -1,10 +1,24 @@
 require("dotenv/config");
 const booksTable = require('../models/book.model')
 const db = require("../db");
-const { eq } = require('drizzle-orm');
+const { eq, ilike } = require('drizzle-orm');
+const { sql } = require("drizzle-orm");
 
 exports.getAllBooks = async function (req, res)
 {
+    const search = req.query.search;
+    // console.log({ search });
+    if (search)
+    {
+        const books = await db
+            .select()
+            .from(booksTable)
+            .where(
+                sql`to_tsvector('english', ${booksTable.title}) @@ to_tsquery('english', ${search})`
+            );
+
+        return res.json(books);
+    }
     const books = await db.select().from(booksTable);
     return res.json(books);
 };
--- 05-Express/book-store/models/book.model.js
+++ 05-Express/book-store/models/book.model.js
@@ -1,11 +1,21 @@
-const { pgTable, uuid, varchar, text } = require("drizzle-orm/pg-core");
+const { pgTable, uuid, varchar, text, index } = require("drizzle-orm/pg-core");
 const authorsTable = require("./author.model");
+const { sql } = require("drizzle-orm");
 
 const booksTable = pgTable("books", {
     id: uuid().primaryKey().defaultRandom(),
     title: text(),
     description: text(),
-    authorId: uuid().references(() => authorsTable.id).notNull(),
-});
+    authorId: uuid()
+        .references(() => authorsTable.id)
+        .notNull(),
+}, 
+(table) => ({
+    searchIndexOnTitle: index("title_index").using(
+        "gin",
+        sql`to_tsvector['english', ${table.title}]`
+    ),
+})
+);
 
 module.exports = booksTable;
