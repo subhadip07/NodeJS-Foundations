0324018799bed793237bf5941ee01e681235ddab
subhadip07 <subhadipbag18@outlook.com>
2025-10-07 21:13:21 +0530

    update: index.js => added route: authorRouter

--- 05-Express/book-store/index.js
+++ 05-Express/book-store/index.js
@@ -1,7 +1,9 @@
 require("dotenv/config");
 const express  = require('express');
 const { loggerMiddleware } = require('./middlewares/logger');
-const bookRouter = require('./routes/book.routes')
+
+const bookRouter = require('./routes/book.routes');
+const authorRouter = require("./routes/author.routes");
 
 const app = express();
 const PORT = 8000;
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use(loggerMiddleware);
 
 // Routes
-app.use('/books', bookRouter);
+app.use("/books", bookRouter);
+app.use("/authors", authorRouter);
 
 app.listen(PORT, () => console.log(`Http server is running on PORT ${PORT}`));
