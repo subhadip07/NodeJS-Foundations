c0675ac8a2510d3f0c728d682b86b7c945715b54
subhadip07 <subhadipbag18@outlook.com>
2025-10-11 21:28:42 +0530

    add: jwt token module

--- 08-Authentication-Session/index.js
+++ 08-Authentication-Session/index.js
@@ -3,37 +3,42 @@ import db from './db/index.js';
 import userRouter from './routes/user.routes.js';
 import {usersTable, userSessions} from './db/schema.js';
 import { eq } from 'drizzle-orm';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.use(express.json());
 app.use(async function(req, res, next){
-    const sessionId = req.headers['session-id'];
-    if (!sessionId)
+    try
     {
-        return next();
+         // Header authorization: Bearer <token>
+        const tokenHeader = req.headers['authorization'];
+
+        if (!tokenHeader)
+        {
+            return next();
+        }
+
+        if (!tokenHeader.startsWith('Bearer'))
+        {
+            return res.status(400).json({ error: 'authorization header must start with Bearer'});
+        }
+
+        const token = tokenHeader.split(' ')[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET, { expiresIn: '1m'});
+
+        req.user = decoded;
+        next();
     }
 
-    const [data] = await db
-        .select({
-            sessionId: userSessions.id,
-            id: usersTable.id,
-            userId: userSessions.userId,
-            name: usersTable.name,
-            email: usersTable.email,
-        })
-        .from(usersTable)
-        .rightJoin(usersTable, eq(usersTable.id, userSessions.userId))
-        .where((table) => eq(table.sessionId, sessionId));
-
-    if (!data)
+    catch(err)
     {
-        return next();
+        next();
     }
-
-    req.user = data;
-    next();
+    
+   
     
 });
 
